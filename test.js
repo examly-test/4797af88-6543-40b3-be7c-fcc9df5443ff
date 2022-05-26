7642ab7d3a4ad941f689c8ee11dcdfae17f064a5
sefeek <safeek@ontoborn.com>
2022-05-26 14:50:21 +0000

    added log

--- test.js
+++ test.js
@@ -2,5 +2,6 @@ console.log('for testing----')
 console.log('for testing-added---')
 console.log('for testing-added--onemore-')
 console.log('for testing-added--onemore-')
+console.log('for testing-added--onemore-')
 
 
